d6d0324899a6a660ed51b93be296d0cf7b7587dd
Graham Ormiston <grahamormiston@gmail.com>
2016-10-28 18:14:13 +0100

    removed about page

--- src/pages/about.php
+++
@@ -1,7 +0,0 @@
-<?php $page_id = "about"; ?>
-<!-- home page meta -->
-<title>Oculus Financial Intelligence - About</title>
-<!-- /end home page meta -->
-</head>
-<body>
-<?php include('index.php') ?>
