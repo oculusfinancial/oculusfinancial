c9b0838380d75081816eb69bc15808a4ca2c44e2
Graham Ormiston <grahamormiston@gmail.com>
2017-02-04 17:58:37 +0000

    Fixes duplicate tabs showing error on how-we-work.php.

--- src/pages/how-we-work.php
+++ src/pages/how-we-work.php
@@ -25,19 +25,19 @@
 <section id="content">
   <div class="text-center">
     <ul class="tabs" data-tabs data-deep-link="true" id="how-we-work-tabs">
-        <li class="tabs-title is-active wow fadeIn" data-wow-duration="2s" data-wow-delay="0.5s"><a href="#methodology">Methodology</a></li>
+        <li class="tabs-title wow fadeIn is-active" data-wow-duration="2s" data-wow-delay="0.5s"><a href="#methodology" aria-selected="true">Methodology</a></li>
         <li class="tabs-title wow fadeIn" data-wow-duration="2s" data-wow-delay="0.7s"><a href="#capability">Capability</a></li>
         <li class="tabs-title wow fadeIn" data-wow-duration="2s" data-wow-delay="0.9s"><a href="#contracting">Contracting With Oculus</a></li>
     </ul>
   </div>
 
 
-  <div class="tabs-content parent-tabs how-we-work-tabs" data-tabs-content="how-we-work-tabs">
+  <div class="tabs-content parent-tabs how-we-work-tabs" data-tabs-content="how-we-work-tabs" data-tabs-content="deeplinked-tabs">
 
-      <div class="tabs-panel how-methodology-background" id="methodology">
+      <div class="tabs-panel how-methodology-background is-active" id="methodology">
         <div class="tint tint-light"></div>
         <div class="row">
-          <div class="wow fadeIn columns small-12 medium-8 small-centered " data-wow-duration="2s" data-wow-delay="0">
+          <div class="wow fadeIn columns small-12 medium-8 small-centered" data-wow-duration="2s" data-wow-delay="0">
               <!-- <h5 class="subheading">Methodology</h5> -->
               <p>Oculus ensures that all its methods of intelligence gathering are legally admissible given the context of the clients’ intentions and do not compromise the outcome or utility to the client of our work. </p>
                 <p>Oculus uses various combinations of the following intelligence gathering disciplines including:</p>
@@ -57,15 +57,15 @@
           <div class="row">
             <div class="wow fadeIn columns small-12 medium-8 small-centered" data-wow-duration="2s" data-wow-delay="0">
                 <p>Oculus deploys specialist Intelligence Analysts who hold current security clearance and are at the leading edge of their skill sets. Intelligence Analysts are professionals sourced from backgrounds in the Security and Intelligence Services, Tier One Investment Bank Anti-Money Laundering (AML) teams and Law Enforcement.</p>
-    			<p>Oculus supports its Intelligence Analysts with the deployment of Financial Analysts, Forensic Accounting Experts and Legal Practitioners.</p>
+                <p>Oculus supports its Intelligence Analysts with the deployment of Financial Analysts, Forensic Accounting Experts and Legal Practitioners.</p>
             </div>
           </div>
         </div>
 
-        <div class="tabs-panel is-active how-contracting-background" id="contracting">
+        <div class="tabs-panel how-contracting-background" id="contracting">
           <div class="tint tint-light"></div>
           <div class="row">
-            <div class="wow fadeIn columns small-12 medium-8 small-centered " data-wow-duration="2s" data-wow-delay="0">
+            <div class="wow fadeIn columns small-12 medium-8 small-centered" data-wow-duration="2s" data-wow-delay="0">
                  <p>Oculus can offer project specific services and evaluates projects for clients on a case by case basis. Clients are quoted costs based upon the forecasted workload. This may be of use if a client has a specific objective in mind.</p>
 
                  <p>In addition to this, Oculus offers retained services. This enables a client to have on demand access to our Intelligence Analysts. This may be of use if a client is undertaking a large enhanced due diligence project on a potential investment, such as a merger or an acquisition. As the upfront work requirements may be unclear it can be more cost effective to retain our services during the period of the transaction. Other clients who are for a period of time carrying out many investment transactions may wish to retain us over an extended period of time.</p>
